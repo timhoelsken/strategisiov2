54270cd10ba36f98e93341781fc454d422975c62
tobwoerk <tobwoerk@9ae889c2-dd43-0410-a83a-a3d442c0eaa8>
2008-01-16 22:01:42 +0000

    + testclass

---
+++ trunk/Strategisio/src/HalloTim.java
@@ -0,0 +1,8 @@
+
+public class HalloTim {
+	public static void main(String[] args) {
+		System.out.println("Hallo Tim.");
+		System.out.println("Wenn du das hier liest, hast du's geschafft.");
+		System.out.println(":D");
+	}
+}
