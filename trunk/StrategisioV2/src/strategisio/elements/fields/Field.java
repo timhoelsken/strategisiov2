45274dce8415eda415cf51f961042bcefba81759
tobwoerk <tobwoerk@9ae889c2-dd43-0410-a83a-a3d442c0eaa8>
2008-01-21 23:02:18 +0000

    + figure to field

--- trunk/StrategisioV2/src/strategisio/elements/fields/Field.java
+++ trunk/StrategisioV2/src/strategisio/elements/fields/Field.java
@@ -1,4 +1,22 @@
 package strategisio.elements.fields;
 
+import strategisio.elements.PlayElement;
+
+/**
+ *
+ * represents a Field on the PlayMap
+ * can hold a PlayElement
+ *
+ */
 public abstract class Field {
+
+	private PlayElement setter = null;
+
+	public PlayElement getSetter() {
+		return setter;
+	}
+
+	public void setSetter(PlayElement setter) {
+		this.setter = setter;
+	}
 }
