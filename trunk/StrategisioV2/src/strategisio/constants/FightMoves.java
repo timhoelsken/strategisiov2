fc48833b4feb1c4dad8fd40a688065b001386b95
tobwoerk <tobwoerk@9ae889c2-dd43-0410-a83a-a3d442c0eaa8>
2008-01-22 20:36:30 +0000

    % moved constants

---
+++ trunk/StrategisioV2/src/strategisio/constants/FightMoves.java
@@ -0,0 +1,30 @@
+package strategisio.constants;
+
+/**
+ * @author Tobias
+ *
+ * has the constants for the attacks & defences in a fight
+ *
+ */
+public class FightMoves {
+
+  /**
+   * BEAT
+   */
+  public static final int BEAT = 0;
+
+  /**
+   * KICK
+   */
+  public static final int KICK = 1;
+
+  /**
+   * CUT
+   */
+  public static final int CUT = 2;
+
+  /**
+   * SHOT
+   */
+  public static final int SHOT = 3;
+}
