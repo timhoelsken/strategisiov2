9c2395e4dedf0ae266ce4dd86c9fc2ad503b4da5
tobwoerk <tobwoerk@9ae889c2-dd43-0410-a83a-a3d442c0eaa8>
2008-01-23 21:22:48 +0000

    + new testcase

--- trunk/StrategisioV2/src/strategisio/tests/PlayMapTest.java
+++ trunk/StrategisioV2/src/strategisio/tests/PlayMapTest.java
@@ -13,6 +13,7 @@ import strategisio.elements.constants.Ground;
 import strategisio.elements.figures.Climber;
 import strategisio.elements.figures.Diver;
 import strategisio.elements.figures.Fighter;
+import strategisio.elements.figures.Spy;
 import strategisio.elements.items.Flag;
 import strategisio.elements.items.Item;
 
@@ -60,12 +61,12 @@ public class PlayMapTest extends TestCase {
    */
   public void testPositioningFigures() throws UnknownFieldTypeException {
     playMap = new PlayMap(1, 3);
-    Fighter tmpFighter = new Fighter();
 
     playMap.setFieldType(0, 0, Ground.GRASS);
     playMap.setFieldType(0, 1, Ground.MOUNTAIN);
     playMap.setFieldType(0, 2, Ground.WATER);
 
+    Fighter tmpFighter = new Fighter();
     assertTrue("Should be possible to set a fighter on a grass field.", playMap.checkPositioningPossibility(tmpFighter, 0, 0));
     assertFalse("Should not be possible to set a fighter on a mountain field.", playMap.checkPositioningPossibility(tmpFighter, 0, 1));
     assertFalse("Should not be possible to set a fighter on a water field.", playMap.checkPositioningPossibility(tmpFighter, 0, 2));
@@ -82,6 +83,37 @@ public class PlayMapTest extends TestCase {
     assertFalse("Should not be possible to set a diver on a mountain field.", playMap.checkPositioningPossibility(tmpDiver, 0, 1));
     assertTrue("Should be possible to set a climber on a water field.", playMap.checkPositioningPossibility(tmpDiver, 0, 2));
     playMap.position(tmpDiver, 0, 2);
+  }
+
+  /**
+   * Tests moving figures of the same team (NULL) on grass.
+   * @throws UnknownFieldTypeException
+   */
+  public void testMovingFigures() throws UnknownFieldTypeException {
+    playMap = new PlayMap(1, 2);
+
+    playMap.setFieldType(0, 0, Ground.GRASS);
+    playMap.setFieldType(0, 1, Ground.GRASS);
+
+    Fighter tmpFighter = new Fighter();
+    playMap.position(tmpFighter, 0, 0);
+    tmpFighter = (Fighter) playMap.fetchSetter(0, 0);
+
+    assertTrue("Should be possible to move the fighter on an empty field.", playMap.checkMovingPossibility(tmpFighter, 0, 1));
+    playMap.move(tmpFighter, 0, 1);
+
+    Spy tmpSpy = new Spy();
+    assertTrue("The field should be free now.", playMap.checkPositioningPossibility(tmpSpy, 0, 0));
+    playMap.position(tmpSpy, 0, 0);
+
+    tmpSpy = (Spy) playMap.fetchSetter(0, 0);
+    assertFalse("Field should be filled by the fighter.", playMap.checkMovingPossibility(tmpSpy, 0, 1));
+    assertTrue("Should be allowed to go back to the same field.", playMap.checkMovingPossibility(tmpSpy, 0, 0));
+    playMap.position(tmpSpy, 0, 0);
+
+    tmpFighter = (Fighter) playMap.fetchSetter(0, 1);
+    assertFalse("Field should be filled by the spy.", playMap.checkMovingPossibility(tmpFighter, 0, 0));
+    playMap.showMap();
   }
 
   /**
