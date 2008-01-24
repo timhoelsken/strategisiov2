d4eeae0a555029747fa1382dab80744b3588187c
tobwoerk <tobwoerk@9ae889c2-dd43-0410-a83a-a3d442c0eaa8>
2008-01-24 18:51:20 +0000

    + checkGround to interface and groundAuthoritIES

--- trunk/StrategisioV2/src/strategisio/elements/PlayMap.java
+++ trunk/StrategisioV2/src/strategisio/elements/PlayMap.java
@@ -6,6 +6,7 @@ import strategisio.elements.fields.Grass;
 import strategisio.elements.fields.Mountain;
 import strategisio.elements.fields.Water;
 import strategisio.elements.figures.Figure;
+import strategisio.elements.items.Item;
 import strategisio.exceptions.UnknownFieldTypeException;
 
 /**
@@ -38,10 +39,6 @@ public class PlayMap {
     initFields(anXDimension, aYDimension);
   }
 
-  private void initFields(int anXDimension, int aYDimension) {
-    fields = new Field[aYDimension][anXDimension];
-  }
-
   /**
    * sets field type for specified field
    *
@@ -68,65 +65,8 @@ public class PlayMap {
     fields[aY][anX] = tmpField;
   }
 
-  /**
-   * Is called with the current field coordinates! Are used to calculate moving
-   * area.
-   *
-   * @param aFigure
-   * @param anX
-   * @param aY
-   */
-  public void selectFieldToMoveTo(Figure aFigure, int anX, int aY) {
-    int[][] tmpMovingArea = getMovingArea(aFigure, anX, aY);
-    int i = 0;
-    // TODO some game logic
-    move(aFigure, tmpMovingArea[i][0], tmpMovingArea[i][1]);
-  }
-
-  /**
-   * Sets the placeAble on the specified field. Checking is necessary before!
-   *
-   * @param aFigure
-   * @param anX
-   * @param aY
-   */
-  private void move(Figure aFigure, int anX, int aY) {
-    if (!checkIfIsEmpty(anX, aY) && checkIfIsEnemy(aFigure, anX, aY)) {
-      Placeable tmpEnemy = getSetter(anX, aY);
-      // TODO differentiate between items and figures
-      tmpEnemy.getId(); // to avoid warning :)
-    } else {
-      // move without problems
-      getField(anX, aY).setSetter(aFigure);
-    }
-  }
-
-  /**
-   * Checks if it is allowed for the Figure to move on the field. FOR INITIAL
-   * SETTING
-   *
-   * @param aFigure
-   * @param anX
-   * @param aY
-   * @return
-   */
-  public boolean checkMovingPossibility(Figure aFigure, int anX, int aY) {
-    return (checkGround(aFigure, anX, aY) && checkIfIsEmptyOrEnemy(aFigure, anX, aY)) ? true : false;
-  }
-
-  /**
-   * Checks if the specified field is empty or filled by an enemy placeAble.
-   */
-  private boolean checkIfIsEmptyOrEnemy(Figure aFigure, int anX, int aY) {
-    return (checkIfIsEmpty(anX, aY) || (checkIfIsEnemy(aFigure, anX, aY))) ? true : false;
-  }
-
-  /**
-   * Checks if there is an enemy on this field.
-   */
-  private boolean checkIfIsEnemy(Figure aFigure, int anX, int aY) {
-    Placeable tmpFieldPlaceable = getSetter(anX, aY);
-    return (aFigure.getId() == tmpFieldPlaceable.getId()) ? false : true;
+  private void initFields(int anXDimension, int aYDimension) {
+    fields = new Field[aYDimension][anXDimension];
   }
 
   /**
@@ -140,106 +80,8 @@ public class PlayMap {
     getField(anX, aY).setSetter(aPlaceable);
   }
 
-  /**
-   * Checks if the given placeAble fits on the ground of the specified field
-   */
-  private boolean checkGround(Placeable aPlaceable, int anX, int aY) {
-    Field tmpField = getField(anX, aY);
-    if (tmpField instanceof Grass){
-      return true;
-    }
-    else{
-
-    if (aPlaceable instanceof Figure) {
-      Figure tmpFigure = (Figure) aPlaceable;
-        if (tmpField instanceof Mountain && tmpFigure.getGroundAuthority() == 1) {
-          return true;
-        } else if (tmpField instanceof Water && tmpFigure.getGroundAuthority() == 2) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    return false;
-
-    }
-  }
-
-  /**
-   * Checks if the specified field is empty
-   */
-  private boolean checkIfIsEmpty(int anX, int aY) {
-    Placeable tmpPlaceable = getSetter(anX, aY);
-    if (tmpPlaceable == null) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  /**
-   * Checks if the given coordinates are inside of the map Dimensions
-   *
-   * @param anX
-   * @param aY
-   * @return true when coordinates are inside of the map Dimensions
-   */
-  private boolean checkCoordinates(int anX, int aY) {
-    if (anX < 0 || anX > getXDimension() || aY < 0 || aY > getYDimension()) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  /**
-   *
-   * @return the xDimension
-   */
-  public int getXDimension() {
-    return fields[0].length;
-  }
-
-  /**
-   *
-   * @return the yDimension
-   */
-  public int getYDimension() {
-    return fields.length;
-  }
-
-  private Field getField(int anX, int aY) {
-    return fields[aY][anX];
-  }
-
-  /**
-   * Gets the setter out of the field (deletes it)
-   *
-   * @param anX
-   * @param aY
-   * @return the setter from the specified field
-   */
-  public Placeable fetchSetter(int anX, int aY) {
-    Placeable tmpPlaceable = getSetter(anX, aY);
-    delSetter(anX, aY);
-    return tmpPlaceable;
-  }
-
-  /**
-   *
-   * @param anX
-   * @param aY
-   * @return a placeAble
-   */
-  public Placeable getSetter(int anX, int aY) {
-    Field tmpField = getField(anX, aY);
-    Placeable tmpPlaceable = tmpField.getSetter();
-    return tmpPlaceable;
-  }
-
-  private void delSetter(int anX, int aY) {
-    Field tmpField = getField(anX, aY);
-    tmpField.setSetter(null);
+  public boolean checkPositioningPossibility(Placeable aPlaceable, int anX, int aY) {
+    return (checkIfIsEmpty(anX, aY) && checkGround(aPlaceable, anX, aY)) ? true : false;
   }
 
   /**
@@ -262,6 +104,7 @@ public class PlayMap {
    * SINGLE FIELD
    *
    * including logic of ground or teamMate in the way
+   *
    * @param aPlaceable
    *
    * @param anX
@@ -273,15 +116,60 @@ public class PlayMap {
   }
 
   /**
+   * Checks if it is allowed for the Figure to move on the field. FOR INITIAL
+   * SETTING
+   *
+   * @param aFigure
+   * @param anX
+   * @param aY
+   * @return
+   */
+  public boolean checkMovingPossibility(Figure aFigure, int anX, int aY) {
+    return (checkGround(aFigure, anX, aY) && checkIfIsEmptyOrEnemy(aFigure, anX, aY)) ? true : false;
+  }
+
+  /**
+   * Is called with the current field coordinates! Use to calculate moving area.
+   *
+   * @param aFigure
+   * @param anX
+   * @param aY
+   */
+  public void selectFieldToMoveTo(Figure aFigure, int anX, int aY) {
+    int[][] tmpMovingArea = getMovingArea(aFigure, anX, aY);
+    int i = 0;
+    // TODO some game logic
+    move(aFigure, tmpMovingArea[i][0], tmpMovingArea[i][1]);
+  }
+
+  /**
+   * Moves the figure onto the specified field. Checking is necessary before!
+   *
+   * @param aFigure
+   * @param anX
+   * @param aY
+   */
+  private void move(Figure aFigure, int anX, int aY) {
+    if (!checkIfIsEmpty(anX, aY) && checkIfIsEnemy(aFigure, anX, aY)) {
+      Placeable tmpEnemy = getSetter(anX, aY);
+      // TODO differentiate between items and figures
+      tmpEnemy.getId(); // to avoid warning :)
+    } else {
+      // move without problems
+      getField(anX, aY).setSetter(aFigure);
+    }
+  }
+
+  /**
    *
    * AREA
    *
    * including logic of moving not further then possible (ground)
    *
-   * @return an array of fields where a figure could be placed
    * @param aFigure
    * @param anX
    * @param aY
+   * @return an array of fields where a figure could be placed
    */
   private int[][] getMovingArea(Figure aFigure, int anX, int aY) {
 
@@ -290,9 +178,7 @@ public class PlayMap {
     int[][] tmpMovingArea = new int[tmpNormalSteps * 4 + tmpDiagonalSteps * 4][1];
     int j = 0;
 
-    /*
-     * All fields to the left
-     */
+    // All fields to the left
     for (int i = 1; i <= tmpNormalSteps; i++) {
       if (checkFigurePositioningPossibility(aFigure, anX - i, aY)) {
         tmpMovingArea[j][0] = anX - i;
@@ -382,5 +268,137 @@ public class PlayMap {
       }
     }
     return tmpMovingArea;
+  }
+
+  /**
+   * Checks if the specified field is empty or filled by an enemy placeAble.
+   */
+  private boolean checkIfIsEmptyOrEnemy(Figure aFigure, int anX, int aY) {
+    return (checkIfIsEmpty(anX, aY) || (checkIfIsEnemy(aFigure, anX, aY))) ? true : false;
+  }
+
+  /**
+   * Checks if there is an enemy on this field.
+   */
+  private boolean checkIfIsEnemy(Figure aFigure, int anX, int aY) {
+    Placeable tmpFieldPlaceable = getSetter(anX, aY);
+    return (aFigure.getId() == tmpFieldPlaceable.getId()) ? false : true;
+  }
+
+  /**
+   * Checks if the given placeAble fits on the ground of the specified field
+   */
+  private boolean checkGround(Placeable aPlaceable, int anX, int aY) {
+    Field tmpField = getField(anX, aY);
+    if (tmpField instanceof Grass) {
+      if (aPlaceable instanceof Item) {
+        return true;
+      } else if (aPlaceable instanceof Figure) {
+        Figure tmpFigure = (Figure) aPlaceable;
+        return checkGround(tmpFigure, Ground.GRASS);
+      }
+    } else if (tmpField instanceof Mountain) {
+      if (aPlaceable instanceof Item) {
+        return false;
+      } else if (aPlaceable instanceof Figure) {
+        Figure tmpFigure = (Figure) aPlaceable;
+        return checkGround(tmpFigure, Ground.MOUNTAIN);
+      }
+    } else if (tmpField instanceof Water) {
+      if (aPlaceable instanceof Item) {
+        return false;
+      } else if (aPlaceable instanceof Figure) {
+        Figure tmpFigure = (Figure) aPlaceable;
+        return checkGround(tmpFigure, Ground.WATER);
+      }
+    }
+    return false;
+  }
+
+  private boolean checkGround(Figure aFigure, int aFieldGround) {
+    int[] tmpGroundAuthorities = aFigure.getGroundAuthorities();
+    for (int i = 0; i < tmpGroundAuthorities.length; i++) {
+      if (tmpGroundAuthorities[i] == aFieldGround) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Checks if the specified field is empty
+   */
+  private boolean checkIfIsEmpty(int anX, int aY) {
+    Placeable tmpPlaceable = getSetter(anX, aY);
+    if (tmpPlaceable == null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private Field getField(int anX, int aY) {
+    return fields[aY][anX];
+  }
+
+  /**
+   *
+   * @param anX
+   * @param aY
+   * @return Placeable
+   */
+  public Placeable getSetter(int anX, int aY) {
+    Field tmpField = getField(anX, aY);
+    Placeable tmpPlaceable = tmpField.getSetter();
+    return tmpPlaceable;
+  }
+
+  /**
+   * Gets the setter out of the field (deletes it)
+   *
+   * @param anX
+   * @param aY
+   * @return the setter from the specified field
+   */
+  public Placeable fetchSetter(int anX, int aY) {
+    Placeable tmpPlaceable = getSetter(anX, aY);
+    delSetter(anX, aY);
+    return tmpPlaceable;
+  }
+
+  private void delSetter(int anX, int aY) {
+    Field tmpField = getField(anX, aY);
+    tmpField.setSetter(null);
+  }
+
+  /**
+   *
+   * @return the xDimension
+   */
+  public int getXDimension() {
+    return fields[0].length;
+  }
+
+  /**
+   *
+   * @return the yDimension
+   */
+  public int getYDimension() {
+    return fields.length;
+  }
+
+  /**
+   * Checks if the given coordinates are inside of the map Dimensions
+   *
+   * @param anX
+   * @param aY
+   * @return true when coordinates are inside of the map Dimensions
+   */
+  private boolean checkCoordinates(int anX, int aY) {
+    if (anX < 0 || anX > getXDimension() || aY < 0 || aY > getYDimension()) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
