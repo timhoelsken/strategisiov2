107ec2bd6aaa3a67fba0e0b137263a8ce6190a10
Tim.Hoelsken <Tim.Hoelsken@9ae889c2-dd43-0410-a83a-a3d442c0eaa8>
2008-01-26 11:21:41 +0000

    + added TestFigure + added GroundMoving Tests

--- trunk/StrategisioV2/src/strategisio/tests/PlayMapTest.java
+++ trunk/StrategisioV2/src/strategisio/tests/PlayMapTest.java
@@ -21,7 +21,7 @@ import strategisio.visualization.ConsoleDisplay;
 
 /**
  * @author Tobias
- *
+ * 
  */
 public class PlayMapTest extends TestCase {
 
@@ -46,7 +46,7 @@ public class PlayMapTest extends TestCase {
 
   /**
    * Tests positioning an item on all three grounds.
-   *
+   * 
    * @throws UnknownFieldTypeException
    */
   public void testPositioningItems() throws UnknownFieldTypeException {
@@ -54,16 +54,19 @@ public class PlayMapTest extends TestCase {
     Item tmpItem = new Flag();
 
     playMap.setFieldType(0, 0, Ground.GRASS);
-    assertTrue("Should be possible to set a flag on a grass field.", playMap.checkPositioningPossibility(tmpItem, 0, 0));
+    assertTrue("Should be possible to set a flag on a grass field.", playMap.checkPositioningPossibility(
+        tmpItem, 0, 0));
     playMap.setFieldType(0, 0, Ground.MOUNTAIN);
-    assertFalse("Should not be possible to set a flag on a mountain field.", playMap.checkPositioningPossibility(tmpItem, 0, 0));
+    assertFalse("Should not be possible to set a flag on a mountain field.", playMap
+        .checkPositioningPossibility(tmpItem, 0, 0));
     playMap.setFieldType(0, 0, Ground.WATER);
-    assertFalse("Should not be possible to set a flag on a water field.", playMap.checkPositioningPossibility(tmpItem, 0, 0));
+    assertFalse("Should not be possible to set a flag on a water field.", playMap.checkPositioningPossibility(
+        tmpItem, 0, 0));
   }
 
   /**
    * Tests positioning every kind of figures on grass.
-   *
+   * 
    * @throws UnknownFieldTypeException
    */
   public void testPositioningFiguresOnGrass() throws UnknownFieldTypeException {
@@ -76,19 +79,21 @@ public class PlayMapTest extends TestCase {
     Fighter tmpFighter = new Fighter();
     Climber tmpClimber = new Climber();
     Diver tmpDiver = new Diver();
-    assertTrue("Should be possible to set a fighter on a grass field.", playMap.checkPositioningPossibility(tmpFighter, 0, 0));
-    assertTrue("Should be possible to set a climber on a grass field.", playMap.checkPositioningPossibility(tmpClimber, 0, 1));
-    assertTrue("Should be possible to set a diver on a grass field.", playMap.checkPositioningPossibility(tmpDiver, 0, 2));
+    assertTrue("Should be possible to set a fighter on a grass field.", playMap.checkPositioningPossibility(
+        tmpFighter, 0, 0));
+    assertTrue("Should be possible to set a climber on a grass field.", playMap.checkPositioningPossibility(
+        tmpClimber, 0, 1));
+    assertTrue("Should be possible to set a diver on a grass field.", playMap.checkPositioningPossibility(
+        tmpDiver, 0, 2));
     playMap.position(tmpFighter, 0, 0);
     playMap.position(tmpClimber, 0, 1);
     playMap.position(tmpDiver, 0, 2);
-    tmpFighter = (Fighter)playMap.fetchSetter(0, 0);
+    tmpFighter = (Fighter) playMap.fetchSetter(0, 0);
   }
-
 
   /**
    * Tests positioning figures on all three grounds.
-   *
+   * 
    * @throws UnknownFieldTypeException
    */
   public void testPositioningFigures() throws UnknownFieldTypeException {
@@ -99,27 +104,34 @@ public class PlayMapTest extends TestCase {
     playMap.setFieldType(0, 2, Ground.WATER);
 
     Fighter tmpFighter = new Fighter();
-    assertTrue("Should be possible to set a fighter on a grass field.", playMap.checkPositioningPossibility(tmpFighter, 0, 0));
-    assertFalse("Should not be possible to set a fighter on a mountain field.", playMap.checkPositioningPossibility(tmpFighter, 0, 1));
-    assertFalse("Should not be possible to set a fighter on a water field.", playMap.checkPositioningPossibility(tmpFighter, 0, 2));
+    assertTrue("Should be possible to set a fighter on a grass field.", playMap.checkPositioningPossibility(
+        tmpFighter, 0, 0));
+    assertFalse("Should not be possible to set a fighter on a mountain field.", playMap
+        .checkPositioningPossibility(tmpFighter, 0, 1));
+    assertFalse("Should not be possible to set a fighter on a water field.", playMap
+        .checkPositioningPossibility(tmpFighter, 0, 2));
     playMap.position(tmpFighter, 0, 0);
 
     Climber tmpClimber = new Climber();
     assertFalse("Should be a fighter there.", playMap.checkPositioningPossibility(tmpClimber, 0, 0));
-    assertTrue("Should be possible to set a climber on a mountain field.", playMap.checkPositioningPossibility(tmpClimber, 0, 1));
-    assertFalse("Should not be possible to set a climber on a water field.", playMap.checkPositioningPossibility(tmpClimber, 0, 2));
+    assertTrue("Should be possible to set a climber on a mountain field.", playMap
+        .checkPositioningPossibility(tmpClimber, 0, 1));
+    assertFalse("Should not be possible to set a climber on a water field.", playMap
+        .checkPositioningPossibility(tmpClimber, 0, 2));
     playMap.position(tmpClimber, 0, 1);
 
     Diver tmpDiver = new Diver();
     assertFalse("Should be a fighter there.", playMap.checkPositioningPossibility(tmpDiver, 0, 0));
-    assertFalse("Should not be possible to set a diver on a mountain field.", playMap.checkPositioningPossibility(tmpDiver, 0, 1));
-    assertTrue("Should be possible to set a climber on a water field.", playMap.checkPositioningPossibility(tmpDiver, 0, 2));
+    assertFalse("Should not be possible to set a diver on a mountain field.", playMap
+        .checkPositioningPossibility(tmpDiver, 0, 1));
+    assertTrue("Should be possible to set a climber on a water field.", playMap.checkPositioningPossibility(
+        tmpDiver, 0, 2));
     playMap.position(tmpDiver, 0, 2);
   }
 
   /**
    * Tests moving figures of the same team (NULL) vertically on grass.
-   *
+   * 
    * @throws UnknownFieldTypeException
    */
   public void testMovingFiguresVerticallyOnGrass() throws UnknownFieldTypeException {
@@ -132,7 +144,8 @@ public class PlayMapTest extends TestCase {
     playMap.position(tmpFighter, 0, 0);
     tmpFighter = (Fighter) playMap.fetchSetter(0, 0);
 
-    assertTrue("Should be possible to move the fighter on an empty field.", playMap.checkMovingPossibility(tmpFighter, 0, 0, 0, 1));
+    assertTrue("Should be possible to move the fighter on an empty field.", playMap.checkMovingPossibility(
+        tmpFighter, 0, 0, 0, 1));
     playMap.move(tmpFighter, 0, 1);
 
     Spy tmpSpy = new Spy();
@@ -142,15 +155,17 @@ public class PlayMapTest extends TestCase {
     tmpSpy = (Spy) playMap.fetchSetter(0, 0);
 
     assertFalse("Field should be filled by the fighter.", playMap.checkMovingPossibility(tmpSpy, 0, 0, 0, 1));
-    assertTrue("Should be allowed to go back to the same field.", playMap.checkMovingPossibility(tmpSpy, 0, 0, 0, 0));
+    assertTrue("Should be allowed to go back to the same field.", playMap.checkMovingPossibility(tmpSpy, 0, 0,
+        0, 0));
 
     tmpFighter = (Fighter) playMap.fetchSetter(0, 1);
-    assertTrue("Should be able to back again because spy is gone.", playMap.checkMovingPossibility(tmpFighter, 0, 1, 0, 0));
+    assertTrue("Should be able to back again because spy is gone.", playMap.checkMovingPossibility(tmpFighter,
+        0, 1, 0, 0));
   }
 
   /**
    * Tests moving figures of the same team (NULL) horizontally on grass.
-   *
+   * 
    * @throws UnknownFieldTypeException
    */
   public void testMovingFiguresHorizontallyOnGrass() throws UnknownFieldTypeException {
@@ -163,7 +178,8 @@ public class PlayMapTest extends TestCase {
     playMap.position(tmpFighter, 0, 0);
     tmpFighter = (Fighter) playMap.fetchSetter(0, 0);
 
-    assertTrue("Should be possible to move the fighter on an empty field.", playMap.checkMovingPossibility(tmpFighter, 0, 0, 1, 0));
+    assertTrue("Should be possible to move the fighter on an empty field.", playMap.checkMovingPossibility(
+        tmpFighter, 0, 0, 1, 0));
     playMap.move(tmpFighter, 1, 0);
 
     Spy tmpSpy = new Spy();
@@ -173,15 +189,17 @@ public class PlayMapTest extends TestCase {
     tmpSpy = (Spy) playMap.fetchSetter(0, 0);
 
     assertFalse("Field should be filled by the fighter.", playMap.checkMovingPossibility(tmpSpy, 0, 0, 1, 0));
-    assertTrue("Should be allowed to go back to the same field.", playMap.checkMovingPossibility(tmpSpy, 0, 0, 0, 0));
+    assertTrue("Should be allowed to go back to the same field.", playMap.checkMovingPossibility(tmpSpy, 0, 0,
+        0, 0));
 
     tmpFighter = (Fighter) playMap.fetchSetter(1, 0);
-    assertTrue("Should be able to back again because spy is gone.", playMap.checkMovingPossibility(tmpFighter, 1, 0, 0, 0));
+    assertTrue("Should be able to back again because spy is gone.", playMap.checkMovingPossibility(tmpFighter,
+        1, 0, 0, 0));
   }
 
   /**
    * Tests moving medics of the same team (NULL) diagonally on grass.
-   *
+   * 
    * @throws UnknownFieldTypeException
    */
   public void testMovingMedicsDiagonallyOnGrass() throws UnknownFieldTypeException {
@@ -196,7 +214,8 @@ public class PlayMapTest extends TestCase {
     playMap.position(tmpMedic1, 0, 0);
     tmpMedic1 = (Medic) playMap.fetchSetter(0, 0);
 
-    assertTrue("Should be possible to move the medic on an empty field.", playMap.checkMovingPossibility(tmpMedic1, 0, 0, 1, 1));
+    assertTrue("Should be possible to move the medic on an empty field.", playMap.checkMovingPossibility(
+        tmpMedic1, 0, 0, 1, 1));
     playMap.move(tmpMedic1, 1, 1);
 
     Medic tmpMedic2 = new Medic();
@@ -205,16 +224,70 @@ public class PlayMapTest extends TestCase {
 
     tmpMedic2 = (Medic) playMap.fetchSetter(0, 0);
 
-    assertFalse("Field should be filled by the other medic.", playMap.checkMovingPossibility(tmpMedic2, 0, 0, 1, 1));
-    assertTrue("Should be allowed to move on an empty field.", playMap.checkMovingPossibility(tmpMedic2, 0, 0, 0, 1));
+    assertFalse("Field should be filled by the other medic.", playMap.checkMovingPossibility(tmpMedic2, 0, 0,
+        1, 1));
+    assertTrue("Should be allowed to move on an empty field.", playMap.checkMovingPossibility(tmpMedic2, 0, 0,
+        0, 1));
     playMap.position(tmpMedic2, 0, 1);
 
     tmpMedic1 = (Medic) playMap.fetchSetter(1, 1);
-    assertTrue("Should be able to back again because other medic is on other field.", playMap.checkMovingPossibility(tmpMedic1, 1, 1, 0, 0));
+    assertTrue("Should be able to back again because other medic is on other field.", playMap
+        .checkMovingPossibility(tmpMedic1, 1, 1, 0, 0));
 
   }
 
+  /**
+   * @throws UnknownFieldTypeException
+   * 
+   */
+  public void testGroundMovingTest() throws UnknownFieldTypeException {
+    playMap = new PlayMap(1, 3);
 
+    playMap.setFieldType(0, 0, Ground.GRASS);
+    playMap.setFieldType(0, 1, Ground.MOUNTAIN);
+    playMap.setFieldType(0, 2, Ground.GRASS);
+
+    Fighter tmpFighter = new Fighter();
+    playMap.position(tmpFighter, 0, 0);
+
+    // Spy tmpSpy = new Spy();
+    // playMap.position(tmpSpy, 0, 2);
+
+    tmpFighter = (Fighter) playMap.fetchSetter(0, 0);
+
+    assertFalse("Should not be possible to move the Fighter behind the mountain field.", playMap
+        .checkMovingPossibility(tmpFighter, 0, 0, 0, 2));
+  }
+
+  /**
+   * @throws UnknownFieldTypeException
+   * 
+   */
+  public void testGroundDiagonalMovingTest() throws UnknownFieldTypeException {
+    playMap = new PlayMap(3, 3);
+
+    playMap.setFieldType(0, 0, Ground.GRASS);
+    playMap.setFieldType(0, 1, Ground.GRASS);
+    playMap.setFieldType(0, 2, Ground.GRASS);
+    playMap.setFieldType(1, 0, Ground.GRASS);
+    playMap.setFieldType(1, 1, Ground.MOUNTAIN);
+    playMap.setFieldType(1, 2, Ground.GRASS);
+    playMap.setFieldType(2, 0, Ground.GRASS);
+    playMap.setFieldType(2, 1, Ground.GRASS);
+    playMap.setFieldType(2, 2, Ground.GRASS);
+
+    TestFigure tmpTestFigure = new TestFigure();
+    playMap.position(tmpTestFigure, 0, 0);
+
+    // Spy tmpSpy = new Spy();
+    // playMap.position(tmpSpy, 0, 2);
+
+    tmpTestFigure = (TestFigure) playMap.fetchSetter(0, 0);
+
+    assertFalse("Should not be possible to move the Fighter behind the mountain field.", playMap
+        .checkMovingPossibility(tmpTestFigure, 0, 0, 2, 2));
+  }
+  
   /**
    * @return the test suite
    */
@@ -224,7 +297,7 @@ public class PlayMapTest extends TestCase {
 
   /**
    * Start the PlayMapTests here.
-   *
+   * 
    * @param args
    */
   public static void main(String[] args) {
