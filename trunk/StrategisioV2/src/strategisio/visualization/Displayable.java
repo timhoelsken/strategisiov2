8fd61de8a0c600cfd89f987c76ddb820bc086c1a
tobwoerk <tobwoerk@9ae889c2-dd43-0410-a83a-a3d442c0eaa8>
2008-01-30 18:12:27 +0000

    + reply

--- trunk/StrategisioV2/src/strategisio/visualization/Displayable.java
+++ trunk/StrategisioV2/src/strategisio/visualization/Displayable.java
@@ -4,20 +4,22 @@ import strategisio.elements.PlayMap;
 
 /**
  * Display interface for painting the game on the screen
- * 
+ *
  * @author Tim
- * 
+ *
  */
 public interface Displayable {
 
   /**
    * To paint the given map on the screen
-   * 
+   *
    * @param aPlayMap
    */
   public void display(PlayMap aPlayMap);
-  
+
   // TODO show enemies in the view of a figure
   // is this the right class for this logic??
-
+  // this is no class. this is an interface. :P
+  // i would put the logic into playmap. where the other logic takes place as
+  // well
 }
