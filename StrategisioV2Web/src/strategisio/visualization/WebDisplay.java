2e8750ecf0de2b3e4c1f73e35b64c3b89377dbc0
tobwoerk <tobwoerk@9ae889c2-dd43-0410-a83a-a3d442c0eaa8>
2008-02-13 21:42:47 +0000

    % nice map

--- StrategisioV2Web/src/strategisio/visualization/WebDisplay.java
+++ StrategisioV2Web/src/strategisio/visualization/WebDisplay.java
@@ -3,6 +3,7 @@ package strategisio.visualization;
 import strategisio.elements.PlayMap;
 import strategisio.elements.constants.Ground;
 import strategisio.elements.fields.Field;
+import strategisio.exceptions.UnknownFieldGroundException;
 
 /**
  * @author Tim
@@ -19,31 +20,24 @@ public class WebDisplay implements Displayable {
   public String display(PlayMap aPlayMap) {
     String tmpOutput = "";
     Field tmpField;
-    tmpOutput = "<div class=\"map\" style=\"width:" + aPlayMap.getXDimension() * 32 + "px;";
-    tmpOutput += "height:" + aPlayMap.getYDimension() * 32 + "px;\">\n";
+    tmpOutput = "<div class=\"map\" style=\"width:" + aPlayMap.getXDimension() * 34.75 + "px; ";
+    tmpOutput += "height:" + aPlayMap.getYDimension() * 34.75 + "px;\">\n";
     // loop for row
     for (int i = 0; i < aPlayMap.getYDimension(); i++) {
-//      tmpOutput = "<div style=\"width:" + aPlayMap.getXDimension() * 32 + "px; height: 32px;";
       // loop for column
-      for (int j = 0, k = 0; j < aPlayMap.getXDimension(); j++) {
+      for (int j = 0; j < aPlayMap.getXDimension(); j++) {
         tmpField = aPlayMap.getField(j, i);
-        if (tmpField.getGround() == Ground.WATER) {
-          tmpOutput += "<div class=\"field water\" ";
-        } else if (tmpField.getGround() == Ground.MOUNTAIN) {
-          tmpOutput += "<div class=\"field mountain\" ";
-        } else {
-          tmpOutput += "<div class=\"field grass\" ";
+        tmpOutput += "<div id=\"" + j + "/" + i + "\" ";
+        try {
+          tmpOutput += "class=\"field " + Ground.getGroundLabeling(tmpField.getGround()) + "\" ";
+        } catch (UnknownFieldGroundException e) {
+          e.printStackTrace();
         }
-        if (j != 0) {
-          k = 1;
-        }
-        tmpOutput += "onMouseOver=\"hoverOn(this);\" onMouseOut=\"hoverOff(this);\" ";
-        tmpOutput += "style=\"margin-left:" + ((j * 32) + (k * 3)) + "px; margin-top:-" + ((k * 32) + (k * 2)) + "px;\">";
+        tmpOutput += "onMouseOver=\"hoverOn(this);\" onMouseOut=\"hoverOff(this);\">";
         tmpOutput += "</div>\n";
       }
-//      tmpOutput += "</div>\n";
     }
-    tmpOutput += "</div>\n\n";
+    tmpOutput += "</div>\n";
 
     return tmpOutput;
   }
