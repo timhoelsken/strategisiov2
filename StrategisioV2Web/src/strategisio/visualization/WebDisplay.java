b41ef48f5f99b6e5c4fbc35760d237d7beabc99e
tobwoerk <tobwoerk@9ae889c2-dd43-0410-a83a-a3d442c0eaa8>
2008-02-13 21:48:05 +0000

    + TODO

--- StrategisioV2Web/src/strategisio/visualization/WebDisplay.java
+++ StrategisioV2Web/src/strategisio/visualization/WebDisplay.java
@@ -20,6 +20,7 @@ public class WebDisplay implements Displayable {
   public String display(PlayMap aPlayMap) {
     String tmpOutput = "";
     Field tmpField;
+    //TODO make internet explorer look as pretty as firefox
     tmpOutput = "<div class=\"map\" style=\"width:" + aPlayMap.getXDimension() * 34.75 + "px; ";
     tmpOutput += "height:" + aPlayMap.getYDimension() * 34.75 + "px;\">\n";
     // loop for row
