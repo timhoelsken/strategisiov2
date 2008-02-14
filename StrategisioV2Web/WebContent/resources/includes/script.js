489e15b9e13fcfa392fc9481d6069aa7aee5360f
tobwoerk <tobwoerk@9ae889c2-dd43-0410-a83a-a3d442c0eaa8>
2008-02-14 21:17:54 +0000

    + make ajax things work...

--- StrategisioV2Web/WebContent/resources/includes/script.js
+++ StrategisioV2Web/WebContent/resources/includes/script.js
@@ -1,62 +1,42 @@
-var http_request = false;
+// Setzen Sie eine geeignete URL ein
+// (beachten Sie dazu die Hinweise im Text):
+// Z.B. http://checkip.dyndns.org
+// oder http://www.myjavaserver.com/~torstenhorn/ajax/checkiptxt.jsp
+// oder checkiptxt.jsp
+var req;
 
-	// Get the URL to the JSP that produces the output, get message (just for test)
-    function macheRequest(url, message) {
-
-        http_request = false;
-
-		//This is for differentiation between IE and others
-		//does not work properly for others than IE, don't know why! have to google a bit
-        if (window.XMLHttpRequest) { // Mozilla, Safari,...
-            http_request = new XMLHttpRequest();
-            if (http_request.overrideMimeType) {
-                http_request.overrideMimeType('text/xml');
-            }
-        } else if (window.ActiveXObject) { // IE
-            try {
-                http_request = new ActiveXObject("Msxml2.XMLHTTP");
-            } catch (e) {
-                try {
-                    http_request = new ActiveXObject("Microsoft.XMLHTTP");
-                } catch (e) {}
-            }
-        }
-
-        if (!http_request) {
-            alert('Ende :( Kann keine XMLHTTP-Instanz erzeugen');
-            return false;
-        }
-        
-    //now work with XMLHTTP instance
-	http_request.onreadystatechange = alertInhalt;
-	
-	//data is send via POST to page in URL
-    http_request.open('POST', url, true);
-    http_request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	//Parameters
-	http_request.send('message=' + message);
+function macheRequest( url, message ) {
+	try {
+		if( window.XMLHttpRequest ) {
+  			req = new XMLHttpRequest();
+    	} else if( window.ActiveXObject ) {
+    		req = new ActiveXObject( "Microsoft.XMLHTTP" );
+    	} else {
+       		alert( "Your Browser doesn't support AJAX!" );
+    	}
+    	req.open( "POST", url, true );
+    	req.onreadystatechange = schreibeAntwort;
+		req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		req.send( 'message=' + message );
+    } catch( e ) {
+      	alert( "Fehler: " + e );
+    }
 }
 
-    
-    function alertInhalt() {
-
-		//readyState changes from 1 to 4 ( 4 means page loaded completly
-		//FF stops at 2 or so
-        if (http_request.readyState == 4) {
-            if (http_request.status == 200) {
-            //this is the place where the response is inserted (output)
-		document.all.output.innerHTML = http_request.responseText;
-            } else {
-                alert('Bei dem Request ist ein Problem aufgetreten.');
-            }
+function schreibeAntwort() {
+	if( 4 == req.readyState ) {
+        if( 200 != req.status ) {
+    		alert( "Request Error " + req.status + ": " + req.statusText );
+        } else {
+	    	document.getElementById("output").innerHTML = req.responseText;
         }
-
     }
+}
 
 function hoverOn(me){
-me.style.borderColor='#ff0000';
+	me.style.borderColor='#ff0000';
 }
 
 function hoverOff(me){
-me.style.borderColor='#ffffff';
+	me.style.borderColor='#ffffff';
 }
