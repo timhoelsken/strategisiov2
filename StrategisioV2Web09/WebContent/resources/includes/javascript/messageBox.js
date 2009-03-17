55d7d0bcb9d1f402b70e3597cf1ba3afd3df7f1c
Tim.Hoelsken@gmail.com <Tim.Hoelsken@gmail.com@9ae889c2-dd43-0410-a83a-a3d442c0eaa8>
2009-03-17 21:12:21 +0000

    % now there is one function to call the msgbox

--- StrategisioV2Web09/WebContent/resources/includes/javascript/messageBox.js
+++ StrategisioV2Web09/WebContent/resources/includes/javascript/messageBox.js
@@ -1,31 +1,24 @@
 
 // standard MessageBox
 function openMessageBox(text){
-	//TODO function has an error
-	var objMessageBox = document.getElementById("messageBox");
-	objMessageBox.onclick = closeMessageBox();
-	objMessageBox.style.width = "100%";
-	objMessageBox.style.height = "100%";
-
-	var mapHeightString = document.getElementById("map").style.height;
-	var mapHeightNumber = mapHeightString.slice(0, mapHeightString.indexOf('p'));
-	var BoxHeight = (parseFloat(mapHeightNumber) + 40);
-
-	var mapWidthString = document.getElementById("map").style.width;
-	var mapWidthNumber = mapWidthString.slice(0, mapWidthString.indexOf('p'));
-	var BoxWidth = (parseFloat(mapWidthNumber) - 31);
-
-	objMessageBox.style.paddingTop = BoxHeight + "px";
-	objMessageBox.style.visibility = "visible";
-	objMessageBox.innerHTML = "<div><table><tr><td><div id=\"centeredTextLeft\"></div></td><td><div id=\"centeredText\"><table><tr><td><img src=\"resources/pictures/exclamationmark.gif\"></td><td>" + text + "</td></tr></table></div></td><td><div id=\"centeredTextRight\"></div></td>";
-	document.getElementById("centeredText").style.width = BoxWidth;
+	openMessageBox()
+	document.getElementById("messageBoxIconPlaceholder").innerHTML = "<img src=\"resources/pictures/wait.gif\" />";
+	document.getElementById("messageBoxTextPlaceholder").innerHTML = text;
+	document.getElementById("overlay").onclick="closeMessageBox();";
+	document.getElementById("messageBox").onclick="closeMessageBox();";
 }
 
 // uncloseable MessageBox
 function openUncloseableMessageBox(text){
-	var objMessageBox = document.getElementById("messageBox");
-	objMessageBox.style.width = "100%";
-	objMessageBox.style.height = "100%";
+	openMessageBox()
+	document.getElementById("messageBoxIconPlaceholder").innerHTML = "<img src=\"resources/pictures/wait.gif\" />";
+	document.getElementById("messageBoxTextPlaceholder").innerHTML = text;
+}
+
+function openMessageBox(){
+	var objOverlay = document.getElementById("overlay");
+	objOverlay.style.width = "100%";
+	objOverlay.style.height = "100%";
 
 	var mapHeightString = document.getElementById("map").style.height;
 	var mapHeightNumber = mapHeightString.slice(0, mapHeightString.indexOf('p'));
@@ -35,16 +28,23 @@ function openUncloseableMessageBox(text){
 	var mapWidthNumber = mapWidthString.slice(0, mapWidthString.indexOf('p'));
 	var BoxWidth = (parseFloat(mapWidthNumber) + 40);
 
-	objMessageBox.style.paddingTop = BoxHeight + "px";
-	objMessageBox.style.visibility = "visible";
-	objMessageBox.innerHTML = "<div><table style=\"border-collapse:collapse;\"><tr><td><div id=\"centeredTextLeft\"></div></td><td><div id=\"centeredText\"><table><tr><td><img src=\"resources/pictures/wait.gif\"></td><td>" + text + "</td></tr></table></div></td><td><div id=\"centeredTextRight\"></div></td>";
-	document.getElementById("centeredText").style.width = BoxWidth;
+	objOverlay.style.paddingTop = BoxHeight + "px";
+	objOverlay.style.visibility = "visible";
+	var messageBoxHTML = "";
+	messageBoxHTML += "<div id=\"messageBox\"><table id=\"messageBoxContentTable\">";
+	messageBoxHTML += "<tr><td class=\"messageBoxContent TopL\"></td><td class=\"messageBoxContent TopC\"></td><td class=\"messageBoxContent TopR\"></td></tr>";
+	messageBoxHTML += "<tr><td class=\"messageBoxContent MiddleL\"></td><td id=\"messageBoxContentPlaceholder\" class=\"messageBoxContent MiddleC\">";
+	messageBoxHTML += "<table><tr><td id=\"messageBoxIconPlaceholder\" class=\"messageBoxPlaceholder Icon\"></td><td id=\"messageBoxTextPlaceholder\" class=\"messageBoxPlaceholder Text\"></td></tr></table></td>";
+	messageBoxHTML += "<td class=\"messageBoxContent MiddleR\"></td></tr>";
+	messageBoxHTML += "<tr><td class=\"messageBoxContent BottomL\"></td><td class=\"messageBoxContent BottomC\"></td><td class=\"messageBoxContent BottomR\"></td></tr>";
+	messageBoxHTML += "</table></div>";
+	objOverlay.innerHTML = messageBoxHTML;
+	document.getElementById("messageBoxContentPlaceholder").style.width = BoxWidth;
 }
-
 // closes the messageBox
 function closeMessageBox(){
-	var objMessageBox = document.getElementById("messageBox");
-	objMessageBox.style.visibility="hidden";
-	objMessageBox.style.width="1px";
-	objMessageBox.style.height="1px";
+	var objOverlay = document.getElementById("overlay");
+	objOverlay.style.visibility="hidden";
+	objOverlay.style.width="1px";
+	objOverlay.style.height="1px";
 }
